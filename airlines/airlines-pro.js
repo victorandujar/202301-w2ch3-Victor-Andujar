e57195f5cf1c7fc3451db227a0b4c9e4fe7acb35
Víctor Andújar <ammavaru@Victors-MacBook-Air.local>
2023-01-18 20:51:52 +0100

    Add eslint to the main project

--- airlines/airlines-pro.js
+++ airlines/airlines-pro.js
@@ -11,199 +11,212 @@ const flights = [
   { id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
 
-let yourName
+let yourName;
 const nameManager = () => {
-  yourName = prompt(`Bienvenido/a a Isdi Coders Airlines \nPara empezar con la búsqueda, porfavor introduce tu nombre`)
+  yourName = prompt(
+    `Bienvenido/a a Isdi Coders Airlines \nPara empezar con la búsqueda, porfavor introduce tu nombre`
+  );
   while (!yourName) {
-    yourName = prompt('Debes introducir un nombre para continuar')
+    yourName = prompt("Debes introducir un nombre para continuar");
   }
-  console.log(`Hola ${yourName}, a continuación te mostramos todos los vuelos:`)
-
-}
+  console.log(
+    `Hola ${yourName}, a continuación te mostramos todos los vuelos:`
+  );
+};
 
 const allFlights = (flights) => {
   for (let i in flights) {
-    const scales = !flights[i].scale ? 'no ' : ''
-    const text = `El vuelo con origen: ${flights[i].from} y destino: ${flights[i].to} tiene un coste de ${flights[i].cost} € y ${scales}hace escala`
-    console.log(text)
+    const scales = !flights[i].scale ? "no " : "";
+    const text = `El vuelo con origen: ${flights[i].from} y destino: ${flights[i].to} tiene un coste de ${flights[i].cost} € y ${scales}hace escala`;
+    console.log(text);
   }
-  return flights
-}
+  return flights;
+};
 
 const averageCost = (flights) => {
-  let acc = 0
+  let acc = 0;
   for (let i in flights) {
-    acc += flights[i].cost
+    acc += flights[i].cost;
   }
-  let totalAverage = acc / flights.length
-  return console.log(`El coste medio de los vuelos es: ${totalAverage}`)
-}
+  let totalAverage = acc / flights.length;
+  return console.log(`El coste medio de los vuelos es: ${totalAverage}`);
+};
 
 const scaleFlights = (flights) => {
-  let scale = []
+  let scale = [];
   for (let i in flights) {
     if (flights[i].scale === true) {
-      scale.push(flights[i])
+      scale.push(flights[i]);
     }
   }
-  return console.log(`Hay ${scale.length} vuelos que hacen escala`)
-}
+  return console.log(`Hay ${scale.length} vuelos que hacen escala`);
+};
 
 const lastFlights = (flights) => {
-  let fiveLast = []
+  let fiveLast = [];
   for (let i in flights) {
     if (i > flights.length - 6) {
-      fiveLast.push(flights[i].to)
+      fiveLast.push(flights[i].to);
     }
   }
-  return console.log(`Los últimos vuelos del día se dirigen a: ${fiveLast.join(', ')}`)
-}
+  return console.log(
+    `Los últimos vuelos del día se dirigen a: ${fiveLast.join(", ")}`
+  );
+};
 
 const priceFlights = (flights) => {
-  let price = Number(prompt('Introduce un precio para empezar la búsqueda'))
+  let price = Number(prompt("Introduce un precio para empezar la búsqueda"));
   for (let i in flights) {
     if (price >= flights[i].cost) {
-      const scales = !flights[i].scale ? 'no ' : ''
-      const search = `El vuelo con origen: ${flights[i].from} y destino: ${flights[i].to} tiene un coste de ${flights[i].cost}€ y ${scales}hace escala`
+      const scales = !flights[i].scale ? "no " : "";
+      const search = `El vuelo con origen: ${flights[i].from} y destino: ${flights[i].to} tiene un coste de ${flights[i].cost}€ y ${scales}hace escala`;
 
-      console.log(search)
+      console.log(search);
     }
   }
-  return flights
-}
+  return flights;
+};
 
 const addFlights = (flights) => {
-  const id2 = flights[flights.length - 1].id + 1
+  const id2 = flights[flights.length - 1].id + 1;
   let newFlights = {
-    id: id2, to: prompt('¿Hacia donde se dirige?'), from: prompt('¿De donde sale el vuelo?'), cost: Number(prompt('¿Cuál es su precio?')), scale: Boolean(prompt('¿Hace escala? true/false'))
-  }
+    id: id2,
+    to: prompt("¿Hacia donde se dirige?"),
+    from: prompt("¿De donde sale el vuelo?"),
+    cost: Number(prompt("¿Cuál es su precio?")),
+    scale: Boolean(prompt("¿Hace escala? true/false")),
+  };
   if (flights.length < 15) {
-    flights.push(newFlights)
+    flights.push(newFlights);
   }
   while (!newFlights.to) {
-    newFlights.to = prompt('¿Hacia donde se dirige?')
+    newFlights.to = prompt("¿Hacia donde se dirige?");
   }
   while (!newFlights.from) {
-    newFlights.from = prompt('¿De donde sale el vuelo?')
+    newFlights.from = prompt("¿De donde sale el vuelo?");
   }
   while (isNaN(newFlights.cost) || !newFlights.cost) {
-    newFlights.cost = Number(prompt('¿Cuál es su precio?'))
+    newFlights.cost = Number(prompt("¿Cuál es su precio?"));
   }
   while (!newFlights.scale) {
-    newFlights.scale = Boolean(prompt('¿Hace escala? true/false'))
+    newFlights.scale = Boolean(prompt("¿Hace escala? true/false"));
   }
-  return console.log(flights)
-}
+  return console.log(flights);
+};
 
 const deleteFlights = (flights) => {
-  const idRemove = Number(prompt('Introduce el ID para borrar los vuelos'))
-  flights.splice(idRemove, 1)
-  alert(`El vuelo con ID: ${idRemove} ha sido eliminado`)
+  const idRemove = Number(prompt("Introduce el ID para borrar los vuelos"));
+  flights.splice(idRemove, 1);
+  alert(`El vuelo con ID: ${idRemove} ha sido eliminado`);
 
-  return console.log(flights)
-}
+  return console.log(flights);
+};
 
 const adminInterface = (flights) => {
-  let keepAdding = ''
-  let keepDeleting = ''
-  let programQuit = ''
-  const addOrDelete = prompt(`Quieres añadir o borrar un vuelo. Escribe tu respuesta para continuar`)
-  if (addOrDelete === 'añadir') {
+  let keepAdding = "";
+  let keepDeleting = "";
+  let programQuit = "";
+  const addOrDelete = prompt(
+    `Quieres añadir o borrar un vuelo. Escribe tu respuesta para continuar`
+  );
+  if (addOrDelete === "añadir") {
     do {
-      addFlights(flights)
-      keepAdding = confirm('Quieres añadir más vuelos?')
+      addFlights(flights);
+      keepAdding = confirm("Quieres añadir más vuelos?");
       if (flights.length === 15) {
-        const noMoreFlights = 'No puedes introducir más vuelos'
-        alert(noMoreFlights)
-        keepAdding = false
+        const noMoreFlights = "No puedes introducir más vuelos";
+        alert(noMoreFlights);
+        keepAdding = false;
       }
       if (keepAdding === false) {
-        let otherAction = confirm('Ahora quieres borrar algún vuelo?')
+        let otherAction = confirm("Ahora quieres borrar algún vuelo?");
         if (otherAction === true) {
           do {
-            deleteFlights(flights)
-            keepDeleting = confirm('Quieres borrar más vuelos')
+            deleteFlights(flights);
+            keepDeleting = confirm("Quieres borrar más vuelos");
             if (keepDeleting === false) {
-              programQuit = 'Hemos terminado por ahora.\nEn la consola encontrarás la base de datos actualizada. Hasta pronto.'
-              alert(programQuit)
+              programQuit =
+                "Hemos terminado por ahora.\nEn la consola encontrarás la base de datos actualizada. Hasta pronto.";
+              alert(programQuit);
             }
-          }
-          while (keepDeleting === true)
+          } while (keepDeleting === true);
         }
         if (otherAction === false) {
-          alert('Gracias por tu tiempo. Hasta pronto.')
+          alert("Gracias por tu tiempo. Hasta pronto.");
         }
       }
-    }
-    while (keepAdding === true)
+    } while (keepAdding === true);
   }
-  if (addOrDelete === 'borrar') {
+  if (addOrDelete === "borrar") {
     do {
-      deleteFlights(flights)
-      keepDeleting = confirm('Quieres borrar más vuelos?')
+      deleteFlights(flights);
+      keepDeleting = confirm("Quieres borrar más vuelos?");
       if (keepDeleting === false) {
-        let otherAction = confirm('Ahora quieres añadir algún vuelo?')
+        let otherAction = confirm("Ahora quieres añadir algún vuelo?");
         if (otherAction === true) {
           do {
-            addFlights(flights)
-            keepAdding = confirm('Quieres añadir más vuelos?')
+            addFlights(flights);
+            keepAdding = confirm("Quieres añadir más vuelos?");
             if (flights.length === 15) {
-              const noMoreFlights = 'No puedes introducir más vuelos'
-              alert(noMoreFlights)
-              keepAdding = false
+              const noMoreFlights = "No puedes introducir más vuelos";
+              alert(noMoreFlights);
+              keepAdding = false;
             }
             if (keepAdding === false) {
-              programQuit = 'Hemos terminado por ahora.\nEn la consola encontrarás la base de datos actualizada. Hasta pronto.'
-              alert(programQuit)
+              programQuit =
+                "Hemos terminado por ahora.\nEn la consola encontrarás la base de datos actualizada. Hasta pronto.";
+              alert(programQuit);
             }
-          }
-          while (keepAdding === true)
+          } while (keepAdding === true);
         }
         if (otherAction === false) {
-          alert('Gracias por tu tiempo. Hasta pronto.')
+          alert("Gracias por tu tiempo. Hasta pronto.");
         }
       }
-    }
-    while (keepDeleting === true)
+    } while (keepDeleting === true);
   }
-  return flights
-}
+  return flights;
+};
 
 const userInterface = (flights) => {
-  let userQuit = ''
+  let userQuit = "";
   do {
-    priceFlights(flights)
-    userQuit = confirm('Quieres realizar una nueva búsqueda')
+    priceFlights(flights);
+    userQuit = confirm("Quieres realizar una nueva búsqueda");
     if (userQuit === false) {
-      const quitMessage = 'Esperamos que hayas encontrado lo que te interesa. Saludos'
-      alert(quitMessage)
+      const quitMessage =
+        "Esperamos que hayas encontrado lo que te interesa. Saludos";
+      alert(quitMessage);
     }
-  }
-  while (userQuit === true)
-  return flights
-}
+  } while (userQuit === true);
+  return flights;
+};
 
 const adminOrUser = () => {
-  let permissions = prompt(`¿Eres admin o user? \nEscribe tu respuesta para poder continuar`)
-  while (permissions !== 'admin' && permissions !== 'user') {
-    permissions = prompt('Lo siento. No te hemos entendido. Eres admin o user?')
+  let permissions = prompt(
+    `¿Eres admin o user? \nEscribe tu respuesta para poder continuar`
+  );
+  while (permissions !== "admin" && permissions !== "user") {
+    permissions = prompt(
+      "Lo siento. No te hemos entendido. Eres admin o user?"
+    );
   }
-  if (permissions === 'admin') {
-    adminInterface(flights)
+  if (permissions === "admin") {
+    adminInterface(flights);
   }
-  if (permissions === 'user') {
-    userInterface(flights)
+  if (permissions === "user") {
+    userInterface(flights);
   }
-  return flights
-}
+  return flights;
+};
 
 const airlines = (flights) => {
-  nameManager()
-  allFlights(flights)
-  averageCost(flights)
-  scaleFlights(flights)
-  lastFlights(flights)
-  adminOrUser(flights)
-}
+  nameManager();
+  allFlights(flights);
+  averageCost(flights);
+  scaleFlights(flights);
+  lastFlights(flights);
+  adminOrUser(flights);
+};
 
-airlines(flights)
+airlines(flights);
